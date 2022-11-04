108c49795ac233cf89835fb975672e573c21b958
Wojciech Mikusek <w.mikusek@exlabs.pl>
2022-11-04 13:11:23 +0100

    Change hosts config

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,7 @@ module RubyRailsPostgres
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.hosts = nil
   end
 end
